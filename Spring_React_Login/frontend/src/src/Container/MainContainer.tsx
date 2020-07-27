191daf4a8cdfd7307c485d72ffdb3a932fb32099
KimDongGeun <ehdrms2034@naver.com>
2020-07-27 20:50:38 +0900

    [Feature] 네아로 로그인 수정과 프론트단 처리

--- Spring_React_Login/frontend/src/src/Container/MainContainer.tsx
+++ Spring_React_Login/frontend/src/src/Container/MainContainer.tsx
@@ -35,6 +35,7 @@ const MainContainer = () => {
     history.push("/signup");
   }
 
+
   return (
     <div>
       <MainPresenter
--- Spring_React_Login/frontend/src/src/Presenter/Main/MainPresenter.tsx
+++ Spring_React_Login/frontend/src/src/Presenter/Main/MainPresenter.tsx
@@ -1,38 +1,46 @@
 import * as React from "react";
 import { Input, Button, Form, Result } from "antd";
 import "./MainPresenter.scss";
-import NaverLogin from 'react-login-by-naver';
-import styled from 'styled-components';
-import naverImg from './naver_login.png';
+import NaverLogin from "react-login-by-naver";
+import styled from "styled-components";
+import naverImg from "./naver_login.png";
 
 type MainPresenterProps = {
-  username: string,
-  setUsername: Function,
-  password: string,
-  setPassword: Function,
-  doLogin: Function,
-  doTest: Function
-}
+  username: string;
+  setUsername: Function;
+  password: string;
+  setPassword: Function;
+  doLogin: Function;
+  doTest: Function;
+};
 
-const NaverImage = styled.div`
-  margin-left : auto;
-  margin-right : auto;
-  background-image : url(${naverImg});
-  background-size : 300px 50px;
-  width : 300px;
-  height : 50px;
-  cursor : pointer;
+const NaverBtn = styled.div`
+  margin-top: 10px;
+  width: 300px;
+  height: 50px;
+  background: #11c800;
+  color: #ffffff;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const MainPresenter = ({
-  username, password,
-  setUsername, setPassword, doLogin, doTest
+  username,
+  password,
+  setUsername,
+  setPassword,
+  doLogin,
+  doTest,
 }: MainPresenterProps) => {
   return (
     <div className={"background"}>
       <Form
         className={"main-container"}
-        onFinish={() => { doLogin(username, password) }}
+        onFinish={() => {
+          doLogin(username, password);
+        }}
       >
         <div className={"loginForm"}>
           아이디
@@ -53,7 +61,7 @@ const MainPresenter = ({
           >
             <Input.Password
               value={password}
-              onChange={e => setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </Form.Item>
           <span className="buttonForm">
@@ -61,30 +69,56 @@ const MainPresenter = ({
               type="primary"
               style={{
                 marginLeft: "auto",
-                marginRight: "10px"
+                marginRight: "10px",
               }}
-              onClick={e=>{doTest();}}
+              onClick={(e) => {
+                doTest();
+              }}
             >
               회원가입
             </Button>
             <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-              >
+              <Button type="primary" htmlType="submit">
                 로그인
               </Button>
             </Form.Item>
           </span>
           <span className="socialLoginForm">
-              <NaverLogin
-                clientId="Rr5h5X4_s0B5nA3TlvQu"
-                callbackUrl="http://localhost:3000"
-                onSuccess={(result)=>{alert(result); console.log(result);}}
-                onFailure={()=>{alert("하이"); console.error();}}
-                render={(props)=> <NaverImage onClick={props.onClick}></NaverImage>}
-              >
-              </NaverLogin>
+            <NaverLogin
+              clientId="Rr5h5X4_s0B5nA3TlvQu"
+              callbackUrl="http://localhost:3000"
+              onSuccess={(result) => {
+                alert(result);
+                console.log(result);
+              }}
+              onFailure={() => {
+                alert("하이");
+                console.error();
+              }}
+              render={(props) => (
+                <NaverBtn onClick={props.onClick}>
+                  네이버 아이디로 로그인
+                </NaverBtn>
+              )}
+            ></NaverLogin>
+
+            <NaverLogin
+              clientId="Rr5h5X4_s0B5nA3TlvQu"
+              callbackUrl="http://localhost:3000"
+              onSuccess={(result) => {
+                alert(result);
+                console.log(result);
+              }}
+              onFailure={() => {
+                alert("하이");
+                console.error();
+              }}
+              render={(props) => (
+                <NaverBtn onClick={props.onClick}>
+                  네이버 아이디로 회원가입
+                </NaverBtn>
+              )}
+            />
           </span>
         </div>
       </Form>
